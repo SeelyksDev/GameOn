3ebf042ae10e49cdc1482b634b823dde3f9dbcdf
SeelyksDev <kevincordier.dev@gmail.com>
2024-11-11 00:22:51 +0100

    feat(signup-form): add success message after form submission

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,122 +1,146 @@
-
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const modalContent = document.querySelector(".content");
+const modalFormBody = document.querySelector(".modal-body");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const closeModalBtn = document.querySelector('.close')
+const closeModalBtn = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
-const form = document.forms['reserve'];
+const form = document.forms["reserve"];
+const confirmationModal = document.querySelector(".sendConfirmation");
+const confirmationTitle = document.querySelector(".confirmation-title");
+const closeConfirmationBtn = document.querySelector(".btn-submit");
 
 // launch and close modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-closeModalBtn.addEventListener('click', closeModal);
+closeModalBtn.addEventListener("click", closeModal);
 
 // Event Validation form
-form.addEventListener("submit", function(e) {
-  if (!validate()) {
-    e.preventDefault();
-  }
+form.addEventListener("submit", function (e) {
+    if (!validate()) {
+        e.preventDefault();
+    } else {
+        e.preventDefault();
+        displayConfirmation();
+    }
 });
+
+closeConfirmationBtn.addEventListener("click", closeConfirmation);
 
 // launch and close modal form
 function launchModal() {
-  modalbg.style.display = "block";
+    modalbg.style.display = "block";
 }
 function closeModal() {
-  modalbg.style.display = "none";
-  clearAllErrors();
+    modalbg.style.display = "none";
+    clearAllErrors();
 }
 
 function validate() {
-  let isValid = true;
+    let isValid = true;
 
-  const firstName = form["first"];
-  if (firstName.value.trim().length < 2) {
-    displayError(firstName, "Le prénom doit contenir au moins 2 caractères.");
-    isValid = false;
-  } else {
-    clearError(firstName);
-  }
+    const firstName = form["first"];
+    if (firstName.value.trim().length < 2) {
+        displayError(
+            firstName,
+            "Le prénom doit contenir au moins 2 caractères."
+        );
+        isValid = false;
+    } else {
+        clearError(firstName);
+    }
 
-  const lastName = form["last"];
-  if (lastName.value.trim().length < 2) {
-    displayError(lastName, "Le nom doit contenir au moins 2 caractères.");
-    isValid = false;
-  } else {
-    clearError(lastName);
-  }
+    const lastName = form["last"];
+    if (lastName.value.trim().length < 2) {
+        displayError(lastName, "Le nom doit contenir au moins 2 caractères.");
+        isValid = false;
+    } else {
+        clearError(lastName);
+    }
 
-  const email = form["email"];
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(email.value)) {
-    displayError(email, "Veuillez entrer une adresse e-mail valide.");
-    isValid = false;
-  } else {
-    clearError(email);
-  }
+    const email = form["email"];
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email.value)) {
+        displayError(email, "Veuillez entrer une adresse e-mail valide.");
+        isValid = false;
+    } else {
+        clearError(email);
+    }
 
-  const birthdate = form['birthdate'];
-  if (!birthdate.value) {
-    displayError(birthdate, "Veuillez entrer votre date de naissance.");
-    isValid = false;
-  } else {
-    clearError(birthdate);
-  }
+    const birthdate = form["birthdate"];
+    if (!birthdate.value) {
+        displayError(birthdate, "Veuillez entrer votre date de naissance.");
+        isValid = false;
+    } else {
+        clearError(birthdate);
+    }
 
-  const quantity = form["quantity"];
-  if (quantity.value === '' || isNaN(quantity.value) || quantity.value < 0) {
-    displayError(quantity, "Veuillez entrer un nombre valide pour les participations.");
-    isValid = false;
-  } else {
-    clearError(quantity);
-  }
+    const quantity = form["quantity"];
+    if (quantity.value === "" || isNaN(quantity.value) || quantity.value < 0) {
+        displayError(
+            quantity,
+            "Veuillez entrer un nombre valide pour les participations."
+        );
+        isValid = false;
+    } else {
+        clearError(quantity);
+    }
 
-  const location = form.querySelector('input[name="location"]:checked');
-  if (!location) {
-    displayError(form["location1"], "Veuillez sélectionner un tournoi.");
-    isValid = false;
-  } else {
-    clearError(form["location1"]);
-  }
+    const location = form.querySelector('input[name="location"]:checked');
+    if (!location) {
+        displayError(form["location1"], "Veuillez sélectionner un tournoi.");
+        isValid = false;
+    } else {
+        clearError(form["location1"]);
+    }
 
-  const termsAccepted = form["checkbox1"];
-  if (!termsAccepted.checked) {
-    displayError(termsAccepted, "Vous devez accepter les conditions d'utilisation.");
-    isValid = false;
-  } else {
-    clearError(termsAccepted);
-  }
+    const termsAccepted = form["checkbox1"];
+    if (!termsAccepted.checked) {
+        displayError(
+            termsAccepted,
+            "Vous devez accepter les conditions d'utilisation."
+        );
+        isValid = false;
+    } else {
+        clearError(termsAccepted);
+    }
 
-
-  return isValid;
+    return isValid;
 }
 
 function displayError(element, message) {
-  const parent = element.closest(".formData");
-  parent.setAttribute("data-error", message);
-  parent.setAttribute("data-error-visible", "true");
+    const parent = element.closest(".formData");
+    parent.setAttribute("data-error", message);
+    parent.setAttribute("data-error-visible", "true");
 }
 
 function clearError(element) {
-  const parent = element.closest(".formData");
-  parent.removeAttribute("data-error");
-  parent.removeAttribute("data-error-visible");
+    const parent = element.closest(".formData");
+    parent.removeAttribute("data-error");
+    parent.removeAttribute("data-error-visible");
 }
 
 function clearAllErrors() {
-  formData.forEach((field) => {
-    field.removeAttribute("data-error");
-    field.removeAttribute("data-error-visible");
-  });
-  form.reset();
+    formData.forEach((field) => {
+        field.removeAttribute("data-error");
+        field.removeAttribute("data-error-visible");
+    });
+    form.reset();
+}
+
+function displayConfirmation() {
+    modalFormBody.style.display = "none";
+    confirmationModal.style.display = "block";
+}
+
+function closeConfirmation() {
+    modalbg.style.display = "none";
 }
 
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
-
-
